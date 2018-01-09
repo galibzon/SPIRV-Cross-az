11b55932fb51c49c18ec36759649621cd4256c9f
Giovanni <giovanni.petrantoni@siliconstudio.co.jp>
2018-01-09 19:32:35 +0900

    more testing code

--- slicc.nim
+++ slicc.nim
@@ -2,9 +2,12 @@
 {.compile:"spirv_cfg.cpp".}
 {.compile:"spirv_glsl.cpp".}
 {.compile:"spirv_hlsl.cpp".}
+{.compile:"spirv_cpp.cpp".}
 import cpp_utilities, gen_spirvcross, math
 proc pushback(vec: spirv, v: uint32) {.importcpp:"#.push_back(#)".}
 proc cstr(str: astring): cstring {.importcpp:"#.c_str()".}
+proc size(res: stdvector_Resource): csize {.importcpp:"#.size()".}
+proc `[]`(res: var stdvector_Resource, index: cint): Resource {.importcpp:"#[#]".}
 
 # ..\tmp\glslang-install\bin\glslangValidator.exe -H -V -o test.spv shaders\frag\basic.frag
 var spirvFile = open("test.spv")
@@ -18,10 +21,62 @@ for i in 0..<spirvFileSize32:
   spirvData.pushback(val[])
 
 var glsl = cppnew initCompilerGLSL(spirvData)
+var glslOptions = initOptions()
+# glslOptions.es = true
+# glslOptions.version = 310
+glslOptions.vulkansemantics = true
+glsl[].setoptions(glslOptions)
+# glsl.AsCompiler()[].buildcombinedimagesamplers()
 var glslCompiled = glsl.AsCompiler()[].compile()
+echo "GLSL:"
 echo glslCompiled.cstr
 
-var hlsl = cppnew initCompilerHLSL(spirvData)
-var hlslCompiled = hlsl[].compile()
-echo hlslCompiled.cstr
+var resources = glsl.AsCompiler()[].getshaderresources()
+
+template printInfo(): untyped =
+  var decorationSet = glsl.AsCompiler()[].getdecoration(resource.id, spvDecoration.DecorationDescriptorSet)
+  var decorationBinding = glsl.AsCompiler()[].getdecoration(resource.id, spvDecoration.DecorationBinding)
+  var decorationLocation = glsl.AsCompiler()[].getdecoration(resource.id, spvDecoration.DecorationLocation)
+  echo resource.name.cstr, " ", $decorationSet, " ", $decorationBinding, " ", $decorationLocation
+
+var sampledImages = resources.sampledimages
+echo "SAMPLED IMAGES:"
+for i in 0..<sampledImages.size:
+  var resource = sampledImages[cint i]
+  printInfo()
+
+var storageImages = resources.storageimages
+echo "STORAGE IMAGES:"
+for i in 0..<storageImages.size:
+  var resource = storageImages[cint i]
+  printInfo()
+
+var separateImages = resources.separateimages
+echo "SEPARATE IMAGES:"
+for i in 0..<separateImages.size:
+  var resource = separateImages[cint i]
+  printInfo()
+  
+var stageInputs = resources.stageinputs
+echo "STAGE INPUTS:"
+for i in 0..<stageInputs.size:
+  var resource = stageInputs[cint i]
+  printInfo()
+
+var stageOutputs = resources.stageoutputs
+echo "STAGE INPUTS:"
+for i in 0..<stageOutputs.size:
+  var resource = stageOutputs[cint i]
+  printInfo()
+
+# var hlsl = cppnew initCompilerHLSL(spirvData)
+# var hlslCompiled = hlsl[].compile()
+# echo "HLSL:"
+# echo hlslCompiled.cstr
+
+# var cpp = cppnew initCompilerCPP(spirvData)
+# cpp.AsCompilerGLSL.AsCompiler()[].buildcombinedimagesamplers()
+# var cppCompiled = cpp[].compile()
+# echo "CPP:"
+# echo cppCompiled.cstr
 
