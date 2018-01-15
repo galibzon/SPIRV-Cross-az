a830fd7b7f48b64909b58e5a62ec95c0274b0f2d
Giovanni <giovanni.petrantoni@siliconstudio.co.jp>
2018-01-15 19:18:05 +0900

    indent fix

--- slicc.nim
+++ slicc.nim
@@ -22,65 +22,65 @@ var {{ShaderName}} = pc.createScript('{{ShaderName}}');
 {{/FloatAttrib}}
 {{#TextureAttrib}}
 {{ShaderName}}.attributes.add('{{TextureName}}', {
-    type: 'asset',
-    assetType: 'texture',
-    title: '{{TextureName}}'
+  type: 'asset',
+  assetType: 'texture',
+  title: '{{TextureName}}'
 });
 
 {{/TextureAttrib}}
 {{ShaderName}}.prototype.reloadShader = function() {
-    var app = this.app;
-    var model = this.entity.model.model;
-    var gd = app.graphicsDevice;
+  var app = this.app;
+  var model = this.entity.model.model;
+  var gd = app.graphicsDevice;
 
-    var vertexShader = `
+  var vertexShader = `
 {{VertexShaderBody}}`;
 
-    var pixelShader = `
+  var pixelShader = `
 {{PixelShaderBody}}`;
 
-    var shaderDefinition = {
-        attributes: {
-            aPosition: pc.SEMANTIC_POSITION,
-            aUv0: pc.SEMANTIC_TEXCOORD0
-        },
-        vshader: vertexShader,
-        fshader: pixelShader
-    };
+  var shaderDefinition = {
+    attributes: {
+      aPosition: pc.SEMANTIC_POSITION,
+      aUv0: pc.SEMANTIC_TEXCOORD0
+    },
+    vshader: vertexShader,
+    fshader: pixelShader
+  };
 
-    // Create the shader from the definition
-    this.shader = new pc.Shader(gd, shaderDefinition);
+  // Create the shader from the definition
+  this.shader = new pc.Shader(gd, shaderDefinition);
 
-    // Create a new material and set the shader
-    this.material = new pc.Material();
-    this.material.setShader(this.shader);
+  // Create a new material and set the shader
+  this.material = new pc.Material();
+  this.material.setShader(this.shader);
 
-    {{#SetFloats}}
-    this.material.setParameter('{{AttribName}}', this.{{AttribName}});
-    this.on('attr:{{AttribName}}', function (value, prev) {
-      this.reloadShader();
-    });
+  {{#SetFloats}}
+  this.material.setParameter('{{AttribName}}', this.{{AttribName}});
+  this.on('attr:{{AttribName}}', function (value, prev) {
+    this.reloadShader();
+  });
 
-    {{/SetFloats}}
-    {{#SetTextures}}
-    this.material.setParameter('{{TextureName}}', this.{{TextureName}}.resource);
-    this.on('attr:{{TextureName}}', function (value, prev) {
-      this.reloadShader();
-    });
+  {{/SetFloats}}
+  {{#SetTextures}}
+  this.material.setParameter('{{TextureName}}', this.{{TextureName}}.resource);
+  this.on('attr:{{TextureName}}', function (value, prev) {
+    this.reloadShader();
+  });
 
-    {{/SetTextures}}
+  {{/SetTextures}}
 
-    // Replace the material on the model with our new material
-    model.meshInstances[0].material = this.material;
+  // Replace the material on the model with our new material
+  model.meshInstances[0].material = this.material;
 };
 
 // initialize code called once per entity
 {{ShaderName}}.prototype.initialize = function() {    
-    this.reloadShader();
+  this.reloadShader();
 };
 
 {{ShaderName}}.prototype.swap = function(old) {
-    this.reloadShader();
+  this.reloadShader();
 };
 """
 
@@ -91,9 +91,9 @@ proc openSpirv(spvFile: string): ptr spirv =
   result = cppnew initspirv()
   assert(spirvFile.readBytes(buffer, 0, spirvFile.getFileSize) == spirvFile.getFileSize)
   for i in 0..<spirvFileSize32:
-    var index = i * 4
-    var val = cast[ptr uint32](addr(buffer[index]))
-    result[].pushback(val[])
+  var index = i * 4
+  var val = cast[ptr uint32](addr(buffer[index]))
+  result[].pushback(val[])
 
 proc close(spv: ptr spirv) =
   deletespirv spv
@@ -129,11 +129,11 @@ proc compileToWebGL1(vertSpv, fragSpv: ptr spirv): string =
   var textureSlots = newSeq[Context]()
   var fragSampledImages = fragResources.sampledimages
   for i in 0..<fragSampledImages.size:
-    var resource = fragSampledImages[cint i]
-    # printInfo fragGlsl
-    var textureAttrib = newContext()
-    textureAttrib["TextureName"] = $resource.name.cstr
-    textureSlots.add(textureAttrib)
+  var resource = fragSampledImages[cint i]
+  # printInfo fragGlsl
+  var textureAttrib = newContext()
+  textureAttrib["TextureName"] = $resource.name.cstr
+  textureSlots.add(textureAttrib)
 
   finalCode["TextureAttrib"] = textureSlots
   finalCode["SetTextures"] = textureSlots
@@ -141,12 +141,12 @@ proc compileToWebGL1(vertSpv, fragSpv: ptr spirv): string =
   var floatSlots = newSeq[Context]()
   var variables = fragGlsl.AsCompiler()[].getactiveinterfacecvariables()
   for vid in cppItems[stdunorderedset_uint32t, uint32](variables):
-    var baseType = @(fragGlsl.AsCompiler()[].gettypecfromcvariable(vid)).basetype
-    if fragGlsl.AsCompiler()[].getstorageclass(vid) == spvStorageClass.StorageClassUniformConstant and baseType == SPIRTypeBaseType.Float:
-      var floatAttrib = newContext()
-      floatAttrib["AttribName"] = $fragGlsl.AsCompiler()[].getname(vid).cstr
-      floatAttrib["DefaultValue"] = "0.0"
-      floatSlots.add(floatAttrib)
+  var baseType = @(fragGlsl.AsCompiler()[].gettypecfromcvariable(vid)).basetype
+  if fragGlsl.AsCompiler()[].getstorageclass(vid) == spvStorageClass.StorageClassUniformConstant and baseType == SPIRTypeBaseType.Float:
+    var floatAttrib = newContext()
+    floatAttrib["AttribName"] = $fragGlsl.AsCompiler()[].getname(vid).cstr
+    floatAttrib["DefaultValue"] = "0.0"
+    floatSlots.add(floatAttrib)
 
   finalCode["FloatAttrib"] = floatSlots
   finalCode["SetFloats"] = floatSlots
